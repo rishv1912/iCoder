cc6fe031e8f3ccf473e4356646935e6e46ba1977
Recxo <rishava065@gmail.com>
2023-04-11 19:04:11 +0530

    added the tiny mce and comments with their replies

--- blog/models.py
+++ blog/models.py
@@ -22,7 +22,7 @@ class BlogComment(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     post=models.ForeignKey(Post, on_delete=models.CASCADE)
     parent=models.ForeignKey('self',on_delete=models.CASCADE, null=True )
-    timestamp= models.DateTimeField(default=now)
+    timestamp= models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.comment[0:13] + "..." + "by " + self.user.username
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
 from blog.models import Post, BlogComment
 from django.contrib import messages
-
+from blog.templatetags import extras
+from django.contrib.auth.models import User
+from django.contrib.auth import  authenticate
 # Create your views here.
 
 # superuse name - coder ,pass - man9 , email - rapx
@@ -11,30 +13,40 @@ def blogHome(request):
     allPosts = Post.objects.all()
     context = {'allPosts': allPosts}
     return render(request, 'blog/blogHome.html', context)
-
-
+  
 def blogPost(request, slug):
     post = Post.objects.filter(slug=slug).first()
-    comments = BlogComment.objects.filter(post=post)
-    context = {'post': post, 'comments': comments,'user':request.user}
+    comments = BlogComment.objects.filter(post=post,parent=None)
+    replies= BlogComment.objects.filter(post=post).exclude(parent=None)
+    replyDict={}
+    for reply in replies:
+        if reply.sno not in replyDict.keys():
+            replyDict[reply.parent.sno]=[reply]
+        else:
+            replyDict[reply.parent.sno].append(reply)
+        
+    context = {'post': post, 'comments': comments,'user':request.user,'replyDict':replyDict}
     return render(request, 'blog/blogPost.html', context)
-
 
 def postComment(request):
     if request.method == "POST":
-        # comment = request.POST.get('comments')
-        # user = request.user
-        # postSno = request.POST.get('postSno')
-        # post = Post.objects.get(sno=postSno)
-
-        comment = request.POST['comment']
-        postSno = request.POST['postSno']
+        comment = request.POST.get('vichar')
         user = request.user
+        postSno = request.POST.get('postSno')
         post = Post.objects.get(sno=postSno)
+        parentSno = request.POST.get('parentSno')
 
-        print(comment, user, postSno, post)
-        comment = BlogComment(comment=comment, user=user, post=post)
-        comment.save()
-        messages.success(request, "Your comment has been posted successfully")
+        
+        if parentSno=='':
+            comment = BlogComment(comment=comment, user=user, post=post)
+            comment.save()
+            messages.success(request, "Your comment has been posted successfully")
+        else:
+            parent = BlogComment.objects.get(sno=parentSno)
+            comment = BlogComment(comment=comment, user=user, post=post,parent=parent)
+        # print(comment, user, postSno, post)
+            comment.save()
+            messages.success(request, "Your reply has been posted successfully")
 
     return redirect(f"/blog/{post.slug}")
+    # return redirect(f"/blog/{post.slug}")
